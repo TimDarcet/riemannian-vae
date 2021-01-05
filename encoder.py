c16aba1dd1821fa92f8f9aa1e86b8a7e53a693d3
Timothée Darcet <timothee.darcet+git@m4x.org>
2021-01-05 11:36:26 +0100

    Don't flattent batch

--- encoder.py
+++ encoder.py
@@ -22,7 +22,7 @@ class LeNet5Encoder(nn.Module):
         x = torch.tanh(self.conv2(x))
         x = self.pool2(x)
         x = torch.tanh(self.conv3(x))
-        x = torch.flatten(x)
+        x = x.view(x.shape[0], -1)
         x = torch.tanh(self.fc1(x))
         x = self.fc2(x)
         return x
